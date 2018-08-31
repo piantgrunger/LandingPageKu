3438ca58d5d5d29bf2a89bd7937a83b8ac041c37
unknown <piant.grunger@gmail.com>
2018-08-31 16:49:09 +0700

    aaa

--- js/script.js
+++ js/script.js
@@ -7,4 +7,38 @@ $('.page-scroll').on('click',function(e){
         },1500,'swing');
     e.preventDefault();
     
+});
+$(window).scroll(function(){
+   var wScroll = $(this).scrollTop();
+
+   $('.jumbotron img').css({
+       'transform' : 'translate(0px,'+wScroll/4+'%) '
+   })
+   $('.jumbotron h1').css({
+    'transform' : 'translate(0px,'+wScroll/2+'%) '
 })
+$('.jumbotron p').css({
+    'transform' : 'translate(0px,'+wScroll/1.2+'%) '
+})
+
+if (wScroll > $('.portfolio').offset().top - 250) {
+    $('.portfolio .thumbnail').each(
+        function(i){
+            setTimeout( function(){
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            },300 * i);
+        }
+    )
+} 
+
+
+if (wScroll > $('.about').offset().top - 250) {
+    console.log('ok');
+
+    $('.about .pKiri').addClass('pMuncul');
+    $('.about .pKanan').addClass('pMuncul');
+
+} ;
+
+
+});
